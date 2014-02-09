ee2f31ddc03702d30655d38d35f3e8d92ac6163d
Peter Reijnders <peter.reijnders@verpeteren.nl>
2014-02-09 13:55:13 +0100

    changed example, to save the output to file correctly

--- example/definitions.mk
+++ example/definitions.mk
@@ -4,7 +4,6 @@ JSDOC2_TEMPLATE=../
 JSDOC2=java -jar $(JSDOC2_INSTALLATION_PATH)/jsrun.jar \
 				 $(JSDOC2_INSTALLATION_PATH)/app/run.js \
 			 -t=$(JSDOC2_TEMPLATE) \
-			 -d=console \
-			 -n \
-			 -l
+			 -d=${DATA_PATH} \
+			 --define="fileName:$(shell basename $@)"
 
